32b8d07ef9a3f9736d284be824e38e61f3a082ee
xLiwiax <boliwia333@gmail.com>
2022-02-23 11:26:24 +0100

    zadanie 12

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let str1 = "Uwielbiam JavaScript";
 let str2 = "Jestem świetnym programistą";
 function fun (a ,b){
-    let dl1 = str1.length;
-    let dl2 = str2.length;
-    if (dl1 > dl2) {
-        return dl1;
+    let dla = a.length;
+    let dlb = b.length;
+    if (dla > dlb) {
+        return a;
     } else{
-        return dl2;
+        return b;
     }
 }
 fun(str1, str2)
